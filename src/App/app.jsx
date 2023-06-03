a970562ec23ae9b10cc4a2affa264df15fbb6c5b
Kristýna Nováková <novakovkri@gmail.com>
2023-06-03 11:47:26 +0200

    adding handleUpVote and handleDown vote function

--- src/App/app.jsx
+++ src/App/app.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 
 export const App = () => {
+  const [upLikes, setUpLikes] = useState(0);
+  const [downLikes, setDownLikes] = useState(0);
+
+  const handleUpVote = () => {
+    setUpLikes(upLikes + 1);
+  };
+
+  const handleDownVote = () => {
+    setDownLikes(downLikes + 1);
+  };
+
   return (
     <div className="container">
       <div className="joke">
@@ -21,13 +32,21 @@ export const App = () => {
           </p>
         </div>
         <div className="joke__likes">
-          <button id="btn-up" className="btn-like btn-like--up"></button>
+          <button
+            id="btn-up"
+            className="btn-like btn-like--up"
+            onClick={handleUpVote}
+          ></button>
           <span id="likes-up" className="likes-count likes-count--up">
-            0
+            {upLikes}
           </span>
-          <button id="btn-down" className="btn-like btn-like--down"></button>
+          <button
+            id="btn-down"
+            className="btn-like btn-like--down"
+            onClick={handleDownVote}
+          ></button>
           <span id="likes-down" className="likes-count likes-count--down">
-            0
+            {downLikes}
           </span>
         </div>
       </div>
